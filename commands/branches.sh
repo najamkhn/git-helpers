ea94ff7e6808564d87d7151877c62822111e8098
Najam Khan <najamkhn@gmail.com>
2014-03-12 11:19:16 +0400

    master: unsetting functions so we can override later

--- commands/branches.sh
+++ commands/branches.sh
@@ -1,3 +1,8 @@
+unset -f git_currentBranch;
+unset -f git_copyCurrentBranchName;
+unset -f git_findMergebyBranchName;
+unset -f git_revertMergeByBranchName;
+
 function git_currentBranch {
 	git branch | grep '*' | sed "s/* //" ;
 }
@@ -8,12 +13,13 @@ function git_copyCurrentBranchName {
 
 function git_findMergebyBranchName {
     export GITH_BRANCH_NAME=$1;
-    git log --grep "Merge.*origin.*`echo $GITH_BRANCH_NAME`";
+    git log --oneline --grep "Merge.*origin.*`echo $GITH_BRANCH_NAME`";
 }
 
-function git_revertMergeByBranchName(){
+function git_revertMergeByBranchName {
     export GITH_BRANCH_NAME=$1;
-    export GITH_REVERT_COMMIT=git log --grep "Merge.*origin.*`echo $GITH_BRANCH_NAME`" | head -n 1 | cut -d ' ' -f1;
-    git revert `echo GITH_REVERT_COMMIT`;
+    export GITH_REVERT_COMMIT_CMD="git log --grep \"Merge.*origin.*`echo $GITH_BRANCH_NAME`\" | head -n 1 | cut -d ' ' -f1";
+    export GITH_REVERT_COMMIT=`eval $GITH_REVERT_COMMIT_CMD`
+    git revert -m 1 `echo $GITH_REVERT_COMMIT`;
 }
 
